dc8552db72106a2d1f17d51bc91e85658378272c
EinfachMoritz119 <76106788+EinfachMoritz119@users.noreply.github.com>
2023-07-18 22:03:01 +0200

    follows now when holding Phone

--- src/main/java/net/prog2s/younesmodid/entity/custom/YounesEntity.java
+++ src/main/java/net/prog2s/younesmodid/entity/custom/YounesEntity.java
@@ -1,10 +1,4 @@
 package net.prog2s.younesmodid.entity.custom;
-
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -14,19 +8,14 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.EnderMan;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.prog2s.younesmodid.entity.ModEntityTypes;
 import net.prog2s.younesmodid.item.ModItems;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib3.GeckoLib;
 import software.bernie.geckolib3.core.IAnimatable;
 
 import software.bernie.geckolib3.core.PlayState;
@@ -43,17 +32,15 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 
 public class YounesEntity extends PathfinderMob implements IAnimatable {
     private final ItemStack heldItem = new ItemStack(ModItems.PHONE.get());
-    private static final EntityDataAccessor<Boolean> DATA_TAMED = SynchedEntityData.defineId(YounesEntity.class, EntityDataSerializers.BOOLEAN);
 
-    private static final String TAG_TAMED = "Tamed";
-    private static final String TAG_OWNER = "OwnerUUID";
+
     private Player owner;
     private static final ItemStack tamingItem = new ItemStack(ModItems.PHONE.get());
 
-    private boolean isTamed = false;
+    private boolean tamed = false;
     private static final ILoopType loop =ILoopType.EDefaultLoopTypes.LOOP;
     private  boolean attacks = false;
-    private AnimationFactory factory = GeckoLibUtil.createFactory(this);
+    private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
     public YounesEntity(EntityType<? extends YounesEntity> entityType, Level level) {
         super(entityType, level);
     }
@@ -72,6 +59,7 @@ public class YounesEntity extends PathfinderMob implements IAnimatable {
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.2D, true));
         this.goalSelector.addGoal(3, new NearestAttackableTargetGoal<EnderMan>(this, EnderMan.class, true,true));
+        this.goalSelector.addGoal(4, new TemptGoal(this, 1.2, Ingredient.of(ModItems.PHONE.get()),false ));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class,1.0F));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
 
@@ -101,10 +89,10 @@ public class YounesEntity extends PathfinderMob implements IAnimatable {
 
 
     @Override
-    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+    public @NotNull InteractionResult mobInteract(Player player, @NotNull InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
         if(itemStack.getItem() == tamingItem.getItem() && !this.isTamed()) {
-            this.isTamed = true;
+            setTamed();
             this.setOwner(player);
             this.setItemSlot(EquipmentSlot.MAINHAND, heldItem);
 
@@ -119,11 +107,11 @@ public class YounesEntity extends PathfinderMob implements IAnimatable {
         return super.mobInteract(player, hand);
     }
 
-    private void setTamed(boolean tamed) {
-    this.entityData.set(DATA_TAMED, tamed);
+    private void setTamed() {
+    this.tamed = true;
     }
     private boolean isTamed() {
-        return isTamed;
+        return tamed;
     }
 
     public void setOwner(Player owner) {
